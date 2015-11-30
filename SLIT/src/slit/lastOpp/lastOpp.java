cae2407c1b3d76015fa1f9b68f7a2374d013c85e
Michael Desmond <michael.m.desmond@gmail.com>
2015-11-30 21:27:48 +0100

    sensur

--- SLIT/src/slit/lastOpp/lastOpp.java
+++ SLIT/src/slit/lastOpp/lastOpp.java
@@ -46,10 +46,8 @@ String filePath=null;
         versjonField = new javax.swing.JTextField();
         lastoppButton = new javax.swing.JButton();
         lastoppExit = new javax.swing.JButton();
-        datoField = new javax.swing.JTextField();
         modulField = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -87,8 +85,6 @@ String filePath=null;
 
         jLabel4.setText("Modulnummer:");
 
-        jLabel5.setText("Dato:");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -98,8 +94,7 @@ String filePath=null;
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
                     .addComponent(jLabel4)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel5))
+                    .addComponent(jLabel3))
                 .addGap(62, 62, 62)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
@@ -107,7 +102,6 @@ String filePath=null;
                     .addComponent(tittelField)
                     .addComponent(lastoppButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(lastoppExit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(datoField)
                     .addComponent(versjonField))
                 .addContainerGap(254, Short.MAX_VALUE))
         );
@@ -129,15 +123,11 @@ String filePath=null;
                             .addComponent(versjonField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3)))
                     .addComponent(jLabel4))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(datoField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5))
-                .addGap(18, 18, 18)
+                .addGap(29, 29, 29)
                 .addComponent(lastoppButton)
                 .addGap(18, 18, 18)
                 .addComponent(lastoppExit)
-                .addContainerGap(142, Short.MAX_VALUE))
+                .addContainerGap(169, Short.MAX_VALUE))
         );
 
         pack();
@@ -211,12 +201,10 @@ String filePath=null;
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField datoField;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JButton lastoppButton;
     private javax.swing.JButton lastoppExit;
     private javax.swing.JTextField modulField;
